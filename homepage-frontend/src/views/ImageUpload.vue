4b5f5e295fa9a24a514aab7570b738af42c2aa94
Shao Qixiang <qixiangshao@mail.ustc.edu.cn>
2021-06-02 20:56:21 +0800

    News Center

---
+++ homepage-frontend/src/views/ImageUpload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-upload class="upload-demo" action="auto" :http-request="uploadSectionFile" list-type="picture">
+      <el-button size="small" type="primary">点击上传</el-button>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2MB</div>
+    </el-upload>
+    <el-button size="small" type="primary" @click="upload">提交</el-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      uploadFile: ""
+    };
+  },
+  methods: {
+   
+  }
+};
+</script>
